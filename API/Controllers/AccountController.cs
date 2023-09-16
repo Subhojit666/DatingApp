d92379c93956a125003fc047e106e0abc7631611
Subhojit666 <subhojitghosh666@gmail.com>
2023-09-16 10:58:32 +0530

    gg

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController : BaseApiController 
+{
+    private readonly DataContext _context;
+    private readonly ITokenService _tokenService;
+
+    public AccountController(DataContext context, ITokenService tokenService )
+    {
+        _context = context;
+        _tokenService = tokenService;
+    }
+[HttpPost("register")]
+public async Task<ActionResult<UserDto>> Register(RegisterDTO registerDTO)
+{
+    if(await UserExists(registerDTO.Username)) return BadRequest("Username is taken!!");
+    using var hmac = new HMACSHA512();
+
+    var user = new AppUser
+    {
+        UserName = registerDTO.Username.ToLower(),
+        PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+        PasswordSalt = hmac.Key
+    };
+
+    _context.Users.Add(user);
+    await _context.SaveChangesAsync();
+
+    return new UserDto
+    {
+        Username = user.UserName,
+        Token = _tokenService.CreateToken(user)
+    };
+}
+
+[HttpPost("login")]
+public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+{
+    var user = await _context.Users.SingleOrDefaultAsync( x=> x.UserName == loginDto.UserName);
+    if (user == null) return Unauthorized("invalid username");
+
+    using var hmac = new HMACSHA512(user.PasswordSalt);
+    var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+    for(int i = 0; i < computedHash.Length; i++)
+    {
+        if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+    }
+
+    return user;
+}
+private async Task<bool> UserExists( string username)
+{
+    return await _context.Users.AnyAsync(x=> x.UserName == username.ToLower());
+}    
+
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,27 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;    
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt=>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
-});
-
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
